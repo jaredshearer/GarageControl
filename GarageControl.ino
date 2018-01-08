b5dc90636d0b02bf0ed01cc35acebf626741f8dd
Jared Shearer <jaredshearer@gmail.com>
2018-01-07 23:06:53 -0600

    Update GarageControl.ino
    First working version.  Need to work on webhooks and/or(???) state change code that's currently commented out.

--- GarageControl.ino
+++ GarageControl.ino
@@ -1,26 +1,27 @@
 /*
 Garage Control
-by Jared Shearer
+
+Copyright 2018 by jsheahawk
 
 This Particle sketch integrates a Particle (Photon in my case) with a Relay Shield with SmartThings.
 
-The Relay Shield uses D0-D3 to actuate the relays.
+The Relay Shield uses D3-D06 to actuate the relays.
 
-The status LED uses PWM to fade an LED to remind you that you left the door open.
+The status LED uses PWM to fade an LED to remind you that you left the door open because extra fancy.  Note: Only some pins are PWM capable.  A4 is one.
 
 */
 
 // Define all the thingz
 int GarageRelay(String command);  // This defines the function that will actually toggle the relays to open the doors.
 
-int g1Relay = D0;    // relay that activates garage 1, 2, or 3
-int g2Relay = D1;
-int g3Relay = D2; 
-// int g4Relay = D3; // This relay isn't being used...but it could be!
+int g1Relay = D3;    // relay that activates garage 1, 2, or 3
+int g2Relay = D4;
+int g3Relay = D5; 
+// int g4Relay = D6; // This relay isn't being used...but it could be!
 
-int g1Switch = D4;   // switch to see if garage 1, 2, or 3 is closed or open
-int g2Switch = D5;
-int g3Switch = D6;
+int g1Switch = D0;   // switch to see if garage 1, 2, or 3 is closed or open
+int g2Switch = D1;
+int g3Switch = D2;
 
 // Defining stuff for that sweet, sweet PWM'ing status LED
 int led = A4;        // define what pin the LED is on (A4 is a PWM pin)
@@ -28,9 +29,12 @@ int brightness = 0;  // how bright the LED is (PWM)
 int fadeAmount = 5;  // how many points (between 0 and 255) to fade the LED by
 
 // Door state sensing declarations used in the loop and by SmartThings
-int g1State = 0;
-int g2State = 0;
-int g3State = 0;
+String g1Status = "unknown";
+String g2Status = "unknown";
+String g3Status = "unknown";
+//int g1State = 0;
+//int g2State = 0;
+//int g3State = 0;
 int g1LastState = 0;
 int g2LastState = 0;
 int g3LastState = 0;
@@ -40,9 +44,9 @@ void setup() {
     pinMode(g2Relay, OUTPUT);
     pinMode(g3Relay, OUTPUT);
     
-    pinMode(g1Switch, INPUT);
-    pinMode(g2Switch, INPUT);
-    pinMode(g3Switch, INPUT);
+    pinMode(g1Switch, INPUT_PULLDOWN);
+    pinMode(g2Switch, INPUT_PULLDOWN);
+    pinMode(g3Switch, INPUT_PULLDOWN);
     
     pinMode(led, OUTPUT);
 
@@ -52,20 +56,42 @@ void setup() {
 
 // Publish toggle functions and door switch variables to the Particle server so SmartThings can see them
     Particle.function("GarageRelay", GarageRelay);
-    Particle.variable("g1State", g1State);
-    Particle.variable("g2State", g2State);
-    Particle.variable("g3State", g3State);
+    Particle.variable("g1Status", g1Status);
+    Particle.variable("g2Status", g2Status);
+    Particle.variable("g3Status", g3Status);
     
 }
 
 void loop() {
     
-    g1State = digitalRead(g1Switch); // Read the state of each door from its respective variable..over and over and over
-    g2State = digitalRead(g2Switch);
-    g3State = digitalRead(g3Switch);
+    int g1State = digitalRead(g1Switch); // Read the state of each door from its respective variable..over and over and over
+    int g2State = digitalRead(g2Switch);
+    int g3State = digitalRead(g3Switch);
+    
+    //int status = digitalRead(microswitch); // read the microswitch pin and store the value to status
+    if (g1State == HIGH){                   // check status if its high store open in the garageContact string
+        g1Status = "Open";
+    }
+    if (g1State == LOW){                   // check status if its low store closed in the garageContact string 
+        g1Status = "Closed";
+    }
+    
+    if (g2State == HIGH){                   // check status if its high store open in the garageContact string
+        g2Status = "Open";
+    }
+    if (g2State == LOW){                   // check status if its low store closed in the garageContact string 
+        g2Status = "Closed";
+    }
+    
+    if (g3State == HIGH){                   // check status if its high store open in the garageContact string
+        g3Status = "Open";
+    }
+    if (g3State == LOW){                   // check status if its low store closed in the garageContact string 
+        g3Status = "Closed";
+    }
     
     
-    
+    /* COMMENTING THIS OUT FOR NOW.  GET LAST STATE UPDATING WORKING LATER
     // These three if/else statements figure out if the door state has changed since the last 
     // go round, and publish the change in state if they have.
     if(g1State == g1LastState){
@@ -73,10 +99,12 @@ void loop() {
     }
     else{ // If they're different, figure out which state it's in and publish its state.
         if(g1State == 0){
-        Particle.publish("Garage 1 closed.");
+        Particle.publish("Garage 1", "Closed");
+        //Particle.variable("g1State", 0);
         }
         else if(g1State == 1){
-        Particle.publish("Garage 1 opened.");
+        Particle.publish("Garage 1", "Opened");
+        //Particle.variable("g1State", 1);
         }        
     }
     
@@ -85,10 +113,12 @@ void loop() {
     }
     else{ // If they're different, figure out which state it's in and publish its state.
         if(g2State == 0){
-        Particle.publish("Garage 2 closed.");
+        Particle.publish("Garage 2", "Closed");
+        //Particle.variable("g2State", 0);
         }
-        else if(g2State == 1){
-        Particle.publish("Garage 2 opened.");
+        else if(g2State != 0){
+        Particle.publish("Garage 2", "Opened");
+        //Particle.variable("g2State", 1);
         }        
     }
     
@@ -97,10 +127,12 @@ void loop() {
     }
     else{ // If they're different, figure out which state it's in and publish its state.
         if(g3State == 0){
-        Particle.publish("Garage 3 closed.");
+        Particle.publish("Garage 3", "Closed");
+        //Particle.variable("g3State", 0);
         }
         else if(g3State == 1){
-        Particle.publish("Garage 3 opened.");
+        Particle.publish("Garage 3", "Opened");
+        //Particle.variable("g3State", 1);
         }        
     }
     
@@ -110,7 +142,7 @@ void loop() {
     g1LastState = g1State;
     g2LastState = g2State;
     g3LastState = g3State;
-    
+    */
     
     
     // LED fading in and out code to remind you that you left the garage door open
