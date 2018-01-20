872961a54bd01cb716197267c7f870b1b34c634b
Jared Shearer <jaredshearer@gmail.com>
2018-01-20 04:35:08 -0600

    Update GarageControl.groovy
    Added openAll and closeAll commands

--- GarageControl.groovy
+++ GarageControl.groovy
@@ -1,25 +1,29 @@
 /*
  *  GarageControl
  *
- *  Copyright 2018 by jsheahawk
+ *  Copyright 2018 by Jared Shearer
  *
  *  This SmartThings device handler integrates a Particle (Photon in my case) with a Relay Shield with SmartThings
  *	to open and monitor the state of garage doors.
  *
- *  TO DO: Webhooks to update the garage door status on a status change.  It's currently JFM to me!
- *
- */
+ *  A WebCoRE piston calls the refresh() function to update the door statuses in SmartThings when a state change 
+ *  is published from the Particle Sketch.
+*/
 
 preferences {
-    input name: "deviceId", type: "text", title: "Device ID", required: true       // Photon device ID (get from build.particle.io)
-    input name: "token", type: "password", title: "Access Token", required: true   // Photon super secret access token (get from build.particle.io)
+    input name: "deviceId", type: "text", title: "Device ID", required: true      // Photon device ID from build.particle.io (you'll put this in the settings) 
+    input name: "token", type: "password", title: "Access Token", required: true  // Photon super secret access token you get from build.particle.io
     input name: "g1Status", type: "text", title: "Garage 1 Status Variable Name", required: true, defaultValue: "g1Status"  // These three lines define the name of the variable
     input name: "g2Status", type: "text", title: "Garage 2 Status Variable Name", required: true, defaultValue: "g2Status"  // that you're looking for from your Particle
     input name: "g3Status", type: "text", title: "Garage 3 Status Variable Name", required: true, defaultValue: "g3Status"  // sketch.
     }
 
 metadata {
-    definition (name: "GarageControl", namespace: "jshearer", author: "Jared Shearer") {
+    definition (name: "GarageControl", namespace: "jaredshearer", author: "Jared Shearer") {
+        capability "Garage Door Control"
+        // capability "actuator"
+        capability "contact sensor"
+        capability "Switch"
         capability "Polling"
         capability "Sensor"
         capability "Refresh"
@@ -31,6 +35,8 @@ metadata {
         command "garage1"  // Defines a command to trigger the garage door we want opened
 		command "garage2"
 		command "garage3"
+        command "openAll"
+        command "closeAll"
     }
 
     tiles(scale: 2) {
@@ -48,11 +54,19 @@ metadata {
 				state "Open", label: "Right", action: "garage3", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
 		}
    
-        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 6, height: 2) {
+        standardTile("openAll", "device.openAll", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+				state "Closed", label: "OPEN ALL", action: "openAll", icon: "", backgroundColor: "#BABFC6"
+				state "Open", label: "OPEN ALL", action: "openAll", icon: "", backgroundColor: "#BABFC6"
+		}
+        standardTile("closeAll", "device.closeAll", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+				state "Closed", label: "CLOSE ALL", action: "closeAll", icon: "", backgroundColor: "#BABFC6"
+				state "Open", label: "CLOSE ALL", action: "closeAll", icon: "", backgroundColor: "#BABFC6"
+		}
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
             state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
         }
 
-		main (["g3Status"])  // This is the tile that will show up on the device's preview.  It's my right garage door in this case
+		main (["g3Status"])  // This is the tile that will show up on the device's preview.  It's my right garage door in this case.
     }
 
 }
@@ -72,7 +86,6 @@ def updated() {
 
 def poll() {  // Polls for the status of the garage doors
     log.debug "Executing 'poll'"
-
     getStatus1()
     getStatus2()
     getStatus3()
@@ -80,7 +93,6 @@ def poll() {  // Polls for the status of the garage doors
 
 def refresh() {  // Calls the garage status functions to refresh them upon refresh button press
     log.debug "Executing 'refresh'"
-
     getStatus1()
     getStatus2()
     getStatus3()
@@ -89,22 +101,26 @@ def refresh() {  // Calls the garage status functions to refresh them upon refre
 def garage1() {  // This is what happens when tile g1Status is pushed.
 	log.debug "Executing 'garage1'"
 	put '1'  // Calls put(GarageRelay) and sends it a "1" which tells the Particle sketch to run GarageRelay with command == 1
-    delay(20000)  // I don't know if this delay then refresh works.  Theoretically, it waits 20s then refreshes the status of the doors
-    refresh()
 }
 
 def garage2() {  // This is what happens when tile g2Status is pushed.
 	log.debug "Executing 'garage2'"
 	put '2'
-    delay(20000)
-    refresh()
 }
 
 def garage3() {  // This is what happens when tile g3Status is pushed.
 	log.debug "Executing 'garage3'"
 	put '3'
-    delay(20000)
-    refresh()
+}
+
+def openAll() {  // This is what happens when tile openAll is pushed.
+	log.debug "Executing 'openAll'"
+	put 'openAll'
+}
+
+def closeAll() {  // This is what happens when tile closeAll is pushed.
+	log.debug "Executing 'closeAll'"
+	put 'closeAll'
 }
 
 // no clue what this does or if its needed
@@ -114,7 +130,7 @@ def parse(String description) {
     createEvent(name: pair[0].trim(), value: pair[1].trim())
 }
 
-// ask smartthings for the garage status
+// How we pull the garage status from Particle
 private getStatus1() {
     def closure = { response ->
         log.debug "request was successful, $response.data"
